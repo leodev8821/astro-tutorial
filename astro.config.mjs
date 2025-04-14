5233766c3987578359dccdb9c0dcbed7db084597
leodev8821 <jaime.leo.caicedo@gmail.com>
2025-04-14 11:13:08 +0200

    Finalizado el ejemplo de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()],
   },
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'client', access: 'public' })
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
     }
   }
 });
